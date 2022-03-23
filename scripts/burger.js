d03d0f79c92bf12d1ad32672329973ba9363ad4e
Lev <salamatinlefff@gmail.com>
2022-03-23 12:07:54 +0400

    refactor: change structure of the document

--- scripts/burger.js
+++ scripts/burger.js
@@ -4,20 +4,20 @@ const burgerMenu = () => {
   const burgerButton = document.querySelector('.burger-menu');
   const overlay = document.querySelector('.overlay');
   const aside = document.querySelector('.aside');
-  const asideCloseButton = aside.querySelector('.aside__button-close');
+  const asideButtonClose = aside.querySelector('.aside__button-close');
 
   const openMenu = () => {
-    aside.classList.add('page__aside--active');
+    aside.classList.add('aside--active');
     overlay.classList.add('overlay--active');
   }
 
   const closeMenu = () => {
-    aside.classList.remove('page__aside--active');
+    aside.classList.remove('aside--active');
     overlay.classList.remove('overlay--active');
   }
 
   burgerButton.addEventListener('click', openMenu);
-  asideCloseButton.addEventListener('click', closeMenu);
+  asideButtonClose.addEventListener('click', closeMenu);
 
   overlay.addEventListener('click', event => {
     if(event.target.classList.contains('overlay')) {
