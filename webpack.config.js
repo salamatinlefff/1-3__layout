7b7157e4e92078e8d51f5924e64f5eea3a27d2d3
Lev <salamatinlefff@gmail.com>
2022-04-05 16:44:24 +0400

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -80,8 +80,8 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp|awif|ico)$/i,
-        type: isProd('asset/resource','asset/inline') ,
-        generator: isProd({filename: 'assets/img/[hash][ext][query]'}, {})
+        type: isProd('asset/resource', 'asset/inline'),
+        generator: isProd({ filename: 'assets/img/[hash][ext][query]' }, {}),
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
